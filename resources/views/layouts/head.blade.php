a94e080015c957b1a7e407230498cb5a2018c7c6
maryamzare <maryam73.zare@gmail.com>
2023-01-08 09:56:30 +0330

    fixed yektanet

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -42,8 +42,8 @@
     <script src="{{ secure_asset('assets/vendor/respond.js') }}"></script>
 <![endif]-->
 <!-- yektanet -->
+<!--script> !function (t, e, n) { t.yektanetAnalyticsObject = n, t[n] = t[n] || function () { t[n].q.push(arguments) }, t[n].q = t[n].q || []; var a = new Date, r = a.getFullYear().toString() + "0" + a.getMonth() + "0" + a.getDate() + "0" + a.getHours(), c = e.getElementsByTagName("script")[0], s = e.createElement("script"); s.id = "ua-script-fr4xWcrA"; s.dataset.analyticsobject = n; s.async = 1; s.type = "text/javascript"; s.src = "https://cdn.yektanet.com/rg_woebegone/scripts_v3/fr4xWcrA/rg.complete.js?v=" + r, c.parentNode.insertBefore(s, c) }(window, document, "yektanet"); </script--> 
 <script> !function (t, e, n) { t.yektanetAnalyticsObject = n, t[n] = t[n] || function () { t[n].q.push(arguments) }, t[n].q = t[n].q || []; var a = new Date, r = a.getFullYear().toString() + "0" + a.getMonth() + "0" + a.getDate() + "0" + a.getHours(), c = e.getElementsByTagName("script")[0], s = e.createElement("script"); s.id = "ua-script-fr4xWcrA"; s.dataset.analyticsobject = n; s.async = 1; s.type = "text/javascript"; s.src = "https://cdn.yektanet.com/rg_woebegone/scripts_v3/fr4xWcrA/rg.complete.js?v=" + r, c.parentNode.insertBefore(s, c) }(window, document, "yektanet"); </script> 
-
 <script type="text/javascript">
   window.RAYCHAT_TOKEN = "a1903fbf-c0f2-41f0-8b21-8894a8405de2";
     (function () {
