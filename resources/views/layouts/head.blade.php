2761585be9e373cfaa7a27629a642db2d656c566
Stack Team <stackteam.official@gmail.com>
2023-01-10 11:57:22 +0330

    change asset sec and change env example

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -20,26 +20,26 @@
   gtag('config', 'G-D0GDEY6W27');
 </script>
 <!-- Favicon -->
-<link rel="icon" type="image/png" sizes="56x56" href="{{ secure_asset('assets/images/fav-icon/icon.png') }}">
+<link rel="icon" type="image/png" sizes="56x56" href="{{ asset('assets/images/fav-icon/icon.png') }}">
 <!-- Main style sheet -->
 @if (in_array(app()->getLocale(),['fa','ar']))
-<link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/css/style-rtl.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style-rtl.css') }}">
 @else
-<link rel="stylesheet" type="text/css" href="{{ secure_asset('assets-ltr/css/style.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets-ltr/css/style.css') }}">
 @endif
 <!-- responsive style sheet -->
-<link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/css/responsive.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/responsive.css') }}">
 <!--  byekan font -->
-<link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/fonts/somar/font.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/fonts/yekan/font.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/fonts/somar/font.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/fonts/yekan/font.css') }}">
 <!-- fix style -->
-<link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/css/custom/custom-style-' . app()->getLocale() . '.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/custom/custom-style-' . app()->getLocale() . '.css') }}">
 
 <!-- Fix Internet Explorer ______________________________________-->
 <!--[if lt IE 9]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-    <script src="{{ secure_asset('assets/vendor/html5shiv.js') }}"></script>
-    <script src="{{ secure_asset('assets/vendor/respond.js') }}"></script>
+    <script src="{{ asset('assets/vendor/html5shiv.js') }}"></script>
+    <script src="{{ asset('assets/vendor/respond.js') }}"></script>
 <![endif]-->
 <!-- yektanet -->
 <!--script> !function (t, e, n) { t.yektanetAnalyticsObject = n, t[n] = t[n] || function () { t[n].q.push(arguments) }, t[n].q = t[n].q || []; var a = new Date, r = a.getFullYear().toString() + "0" + a.getMonth() + "0" + a.getDate() + "0" + a.getHours(), c = e.getElementsByTagName("script")[0], s = e.createElement("script"); s.id = "ua-script-fr4xWcrA"; s.dataset.analyticsobject = n; s.async = 1; s.type = "text/javascript"; s.src = "https://cdn.yektanet.com/rg_woebegone/scripts_v3/fr4xWcrA/rg.complete.js?v=" + r, c.parentNode.insertBefore(s, c) }(window, document, "yektanet"); </script--> 
