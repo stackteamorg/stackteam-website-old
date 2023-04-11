eed2f753197a60b8f1c2d3d0672cf2d8ac735531
maryamzare <maryam73.zare@gmail.com>
2023-04-11 12:59:27 +0330

    add mx class to menu

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,5 +1,5 @@
 @if( $routeName != $link )
-<li class="nav-item"> <a class="nav-link" href="{{ $link }}">{{ $text }} {{$routname}}</a> </li>
+<li class="nav-item mx-2"> <a class="nav-link" href="{{ $link }}">{{ $text }} {{$routname}}</a> </li>
 @else
-<li class="nav-item"> <a class="nav-link active" href="{{ $link }}"><b>{{ $text }} {{$routname}}</b></a> </li>
+<li class="nav-item mx-2"> <a class="nav-link active" href="{{ $link }}"><b>{{ $text }} {{$routname}}</b></a> </li>
 @endif
